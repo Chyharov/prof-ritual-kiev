f65fbff700b671b813f5abe15a3fee2e7e1b29e5
myusername <chyharov@gmail.com>
2023-11-02 22:23:08 +0200

    ADDED: logo for Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
+import Logo from '../Logo/Logo'
 import s from './Header.module.scss';
 
 const Header = () => {
   return (
     <header className={s.header}>
       <div className={'container ' + s.header__padding}>
-
-        <h1 className={s.header__title}>Header</h1>
+        <Logo />
         
       </div>
     </header>
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo/profritual.png';
+import s from './Logo.module.scss';
+
+const Logo = () => {
+    return (
+    <Link className={s.logo} to="/">
+      <img className={s.logo__img} loading="lazy" src={logo} alt="header__logo" />
+    </Link>
+  )
+};
+
+export default Logo;
