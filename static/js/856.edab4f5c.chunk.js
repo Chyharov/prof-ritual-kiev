78ad47e2711e5957002b32bba718db2695631a8d
Chyharov <102535915+Chyharov@users.noreply.github.com>
2023-11-06 22:27:44 +0000

    Deploying to gh-pages from @ Chyharov/prof-ritual-kiev@43249996b0c0e87085ec26afd4022272de455056 🚀

---
+++ static/js/856.edab4f5c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprof_ritual_kiev=self.webpackChunkprof_ritual_kiev||[]).push([[856],{6856:function(t,a,r){r.d(a,{sG5:function(){return e}});var n=r(9983);function e(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M16.01 11H4v2h12.01v3L20 12l-3.99-4z"}}]})(t)}}}]);
+//# sourceMappingURL=856.edab4f5c.chunk.js.map
