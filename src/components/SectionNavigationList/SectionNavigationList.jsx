2ca974f49245b71fbcc9b28c79e0d97e7dc14be5
myusername <chyharov@gmail.com>
2023-11-18 01:43:17 +0200

    UPDATE: SectionNavigationList added react-router-dom for list items

--- src/components/SectionNavigationList/SectionNavigationList.jsx
+++ src/components/SectionNavigationList/SectionNavigationList.jsx
@@ -1,40 +1,42 @@
+import { Link } from 'react-router-dom';
 import s from './SectionNavigationList.module.scss'
+
+
 
 const SectionNavigationList = () => {
     return (
       <section className={s.sectionNavigationList}>
         <div className={'container ' + s.navigationList__container}>
             
-        <h2 className={s.navigationList__title}>Ритуальні послуги</h2>
-        
-        <ul className={s.navigationList__list}>
-            <li className={s.navigationList__item}>Ритуальний агент</li>
-            <li className={s.navigationList__item}>Організація похорон</li>
-            <li className={s.navigationList__item}>Кремація</li>
-            <li className={s.navigationList__item}>Автокатафалк</li>
-            <li className={s.navigationList__item}>Вантаж 200</li>
-            <li className={s.navigationList__item}>Послуги моргу</li>
-            <li className={s.navigationList__item}>Поминальні обіди</li>
-            <li className={s.navigationList__item}>Оформлення документів</li>
-            <li className={s.navigationList__item}>Ритуальна зала</li>
-            <li className={s.navigationList__item}>Музичний супровід</li>
-            <li className={s.navigationList__item}>Винесення тіла</li>
-            <li className={s.navigationList__item}>Відспівування померлого</li>
-            <li className={s.navigationList__item}>Санобробка приміщень</li>
-            <li className={s.navigationList__item}>Виготовлення фотопортрету</li>
-            <li className={s.navigationList__item}>Супровід медперсоналом</li>
-        </ul>
-        
-        <h2 className={s.navigationList__title}>Ритуальні товари</h2>
+        <h2 className={s.navigationList__title}><Link to="/ritualservices">Ритуальні послуги</Link></h2>
                 
         <ul className={s.navigationList__list}>
-            <li className={s.navigationList__item}>Вінки</li>
-            <li className={s.navigationList__item}>Труни</li>
-            <li className={s.navigationList__item}>Хрести</li>
-            <li className={s.navigationList__item}>Пам’ятники</li>
-            <li className={s.navigationList__item}>Приладдя</li>
+            <li className={s.navigationList__item}><Link to="/ritualagent">Ритуальний агент</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Організація похорон</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Кремація</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Автокатафалк</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Вантаж 200</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Послуги моргу</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Поминальні обіди</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Оформлення документів</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Ритуальна зала</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Музичний супровід</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Винесення тіла</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Відспівування померлого</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Санобробка приміщень</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Виготовлення фотопортрету</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Супровід медперсоналом</Link></li>
         </ul>
-
+        
+        <h2 className={s.navigationList__title}><Link to="/ritualgoods">Ритуальні товари</Link></h2>
+                
+        <ul className={s.navigationList__list}>
+            <li className={s.navigationList__item}><Link to="/">Вінки</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Труни</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Хрести</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Пам’ятники</Link></li>
+            <li className={s.navigationList__item}><Link to="/">Приладдя</Link></li>
+        </ul>
 
         </div>
       </section>
