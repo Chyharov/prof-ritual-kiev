063abd67518b8d5e6b95cb0a6ca723f118c67b00
Oleksii Chyharov <chyharov@gmail.com>
2023-12-05 21:59:38 +0200

    CHANGE: phone numbers for SectionContactsList

--- src/components/SectionContactsList/SectionContactsList.jsx
+++ src/components/SectionContactsList/SectionContactsList.jsx
@@ -1,16 +1,14 @@
 import s from './SectionContactsList.module.scss';
 
-
 const SectionContactsList = () => {
 
   return (
     <section className={s.sectionContactsList}>
       <div className={'container ' + s.sectionContactsList__container}>
         <ul className={s.sectionContactsList_list}>
-          <li className={s.sectionContactsList_item}><a href="tel:+38(044)2092472">+38 (044) 209-24-72</a></li>
-          <li className={s.sectionContactsList_item}><a href="tel:+38(099)6813773">+38 (099) 681-37-73</a></li>
-          <li className={s.sectionContactsList_item}><a href="tel:+38(073)6813773">+38 (073) 681-37-73</a></li>
-          <li className={s.sectionContactsList_item}><a href="tel:+38(098)5813773">+38 (098) 581-37-73 </a></li>
+          <li className={s.sectionContactsList_item}><a href="tel:+38(044)2090261">+38 (044) 209-02-61</a></li>
+          <li className={s.sectionContactsList_item}><a href="tel:+38(067)0112160">+38 (067) 011-21-60</a></li>
+          <li className={s.sectionContactsList_item}><a href="tel:+38(093)9040707">+38 (093) 904-07-07</a></li>
         </ul>
       </div>
     </section>
