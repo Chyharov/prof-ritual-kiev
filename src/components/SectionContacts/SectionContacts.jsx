162415528ea2cfd4b1101f89b9ce7666dafa5c40
Oleksii Chyharov <chyharov@gmail.com>
2023-12-05 22:06:09 +0200

    CHANGE: phone numbers for SectionContacts

--- src/components/SectionContacts/SectionContacts.jsx
+++ src/components/SectionContacts/SectionContacts.jsx
@@ -7,13 +7,12 @@ const SectionContacts = () => {
         <div className={'container ' + s.contacts__container}>
                 
                 <button className={s.contacts__btn}>Викликати агента</button>
-                <a className={s.contacts__call} href="tel:+38(044)209-24-72">+38 (044) 209-24-72</a>
+                <a className={s.contacts__call} href="tel:+38(044)209-02-61">+38 (044) 209-02-61</a>
 
                 <h2 className={s.contacts__title}>Телефони:</h2>
                 <ul className={s.contacts__list}>
-                    <li className={s.contacts__item}><a href="tel:+38(099)6813773">+38 (099) 681-37-73</a></li>
-                    <li className={s.contacts__item}><a href="tel:+38(073)6813773">+38 (073) 681-37-73</a></li>
-                    <li className={s.contacts__item}><a href="tel:+38(098)5813773">+38 (098) 581-37-73 </a></li>
+                    <li className={s.contacts__item}><a href="tel:+38(099)6813773">+38 (067) 011-21-60</a></li>
+                    <li className={s.contacts__item}><a href="tel:+38(073)6813773">+38 (093) 904-07-07</a></li>
                 </ul>
           
                 <div className={s.contacts__border}></div>
