827fe5f958f183b43f7dcd914cda5af9c7c00da3
Oleksii Chyharov <chyharov@gmail.com>
2023-12-06 02:05:35 +0200

    DONE: modalWindow for SectionContacts is ready

--- src/components/SectionContacts/SectionContacts.jsx
+++ src/components/SectionContacts/SectionContacts.jsx
@@ -1,39 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import { FaTelegram } from "react-icons/fa6";
+import { FaViber } from "react-icons/fa";
+import { FaSquareWhatsapp } from "react-icons/fa6";
+import { IoMdClose } from "react-icons/io";
 import s from './SectionContacts.module.scss';
 
 const SectionContacts = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
 
-  const handleModalToggle = () => {
-    setIsModalOpen(!isModalOpen);
+  const openModal = () => {
+    setModalOpen(true);
+    document.body.style.overflow = 'hidden';
   };
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Escape') {
-      setIsModalOpen(false);
+  const closeModal = () => {
+    setModalOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
+  const handleOutsideClick = (event) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
     }
   };
 
-  const handleContainerClick = (e) => {
-    if (e.target.classList.contains(s.modal)) {
-      setIsModalOpen(false);
+  const handleKeyDown = (event) => {
+    if (event.key === 'Escape') {
+      closeModal();
     }
   };
 
   useEffect(() => {
-    window.addEventListener('keydown', handleKeyPress);
-    document.addEventListener('click', handleContainerClick);
+    window.addEventListener("keydown", handleKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyPress);
-      document.removeEventListener('click', handleContainerClick);
+      window.removeEventListener("keydown", handleKeyDown);
     };
-  }, []);
+  });
 
   return (
     <section className={s.sectionContacts}>
       <div className={'container ' + s.contacts__container}>
-        <button className={s.contacts__btn} onClick={handleModalToggle}>
+        <button className={s.contacts__btn} onClick={openModal}>
           Викликати агента
         </button>
         <a className={s.contacts__call} href="tel:+38044209-02-61">
@@ -53,14 +61,32 @@ const SectionContacts = () => {
         <div className={s.contacts__border}></div>
 
         {isModalOpen && (
-          <div className={s.modal} onClick={handleContainerClick}>
-            <ul>
-              <li><p><a href="https://t.me/+380672459505" target="_blank" rel="noopener noreferrer">Telegram</a></p></li>
-              <li><p><a href="viber://chat?number=+380672459505" target="_blank" rel="noopener noreferrer">Viber</a></p></li>
-              <li><p><a href="whatsapp://send?phone=+380672459505" target="_blank" rel="noopener noreferrer">WhatsApp</a></p></li>
-            </ul>
+          <div className={s.modal} onClick={handleOutsideClick}>
+            <IoMdClose className={s.btn__close} onClick={closeModal} />
+            <ul className={s.contact__list}>
+              
+              <li className={s.contact__item}>
+                <a className={s.contact__links} href="https://t.me/+380672459505" target="_blank" rel="noopener noreferrer">
+                  <FaTelegram className={`${s.contact__icons} ${s.icon__telegram}`} />
+                  <p> Telegram</p>
+                </a>
+              </li>
 
-            <button onClick={handleModalToggle}>Закрити модалку</button>
+              <li className={s.contact__item}>
+                <a className={s.contact__links} href="viber://chat?number=+380672459505/" target="_blank" rel="noopener noreferrer">
+                  <FaViber className={`${s.contact__icons} ${s.icon__viber}`} />
+                  <p> Viber</p>
+                </a>
+              </li>
+
+              <li className={s.contact__item}>
+                <a className={s.contact__links} href="whatsapp://send?phone=+380672459505" target="_blank" rel="noopener noreferrer">
+                  <FaSquareWhatsapp className={`${s.contact__icons} ${s.icon__whatsApp}`} />
+                  <p> WhatsApp</p>
+                </a>
+              </li>
+
+            </ul>
           </div>
         )}
       </div>
