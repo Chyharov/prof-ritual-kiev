fe424a1ac91f8f457d0320ee2c643dd90f9534d5
myusername <chyharov@gmail.com>
2023-12-10 23:44:47 +0200

    FIX: transfer nav links

--- src/components/SectionNavigationList/SectionNavigationList.jsx
+++ src/components/SectionNavigationList/SectionNavigationList.jsx
@@ -8,34 +8,34 @@ const SectionNavigationList = () => {
       <section className={s.sectionNavigationList}>
         <div className={'container ' + s.navigationList__container}>
             
-        <h2 className={s.navigationList__title}><Link to="/ritualservices">Ритуальні послуги</Link></h2>
+        <h2 className={s.navigationList__title}><Link className={s.navigationList__link} to="/ritualservices">Ритуальні послуги</Link></h2>
                 
-        <ul className={s.navigationList__list}>
-            <li className={s.navigationList__item}><Link to="/ritualagent">Ритуальний агент</Link></li>
-            <li className={s.navigationList__item}><Link to="/funeralorganization">Організація похорон</Link></li>
-            <li className={s.navigationList__item}><Link to="/cremation">Кремація</Link></li>
-            <li className={s.navigationList__item}><Link to="/hearse">Автокатафалк</Link></li>
-            <li className={s.navigationList__item}><Link to="/cargo">Вантаж 200</Link></li>
-            <li className={s.navigationList__item}><Link to="/mortuaryservices">Послуги моргу</Link></li>
-            <li className={s.navigationList__item}><Link to="/memorialdinners">Поминальні обіди</Link></li>
-            <li className={s.navigationList__item}><Link to="/documentsforfuneral">Оформлення документів для похорону</Link></li>
-            <li className={s.navigationList__item}><Link to="/ritualhall">Ритуальна зала</Link></li>
-            <li className={s.navigationList__item}><Link to="/musicalaccompanimen">Музичний супровід</Link></li>
-            <li className={s.navigationList__item}><Link to="/removalbody">Винесення тіла</Link></li>
-            <li className={s.navigationList__item}><Link to="/requiemservicebypriest">Відспівування померлого</Link></li>
-            <li className={s.navigationList__item}><Link to="/sanitationofpremises">Санобробка приміщень</Link></li>
-            <li className={s.navigationList__item}><Link to="/makingfuneralportrait">Виготовлення фотопортрету</Link></li>
-            <li className={s.navigationList__item}><Link to="/funeralescortbymedicalpersonnal">Супровід медперсоналом</Link></li>
+          <ul className={s.navigationList__list}>
+            <Link className={s.navigationList__link} to="/ritualagent"><li className={s.navigationList__item}>Ритуальний агент</li></Link>
+            <Link className={s.navigationList__link} to="/funeralorganization"><li className={s.navigationList__item}>Організація похорон</li></Link>
+            <Link className={s.navigationList__link} to="/cremation"><li className={s.navigationList__item}>Кремація</li></Link>
+            <Link className={s.navigationList__link} to="/hearse"><li className={s.navigationList__item}>Автокатафалк</li></Link>
+            <Link className={s.navigationList__link} to="/cargo"><li className={s.navigationList__item}>Вантаж 200</li></Link>
+            <Link className={s.navigationList__link} to="/mortuaryservices"><li className={s.navigationList__item}>Послуги моргу</li></Link>
+            <Link className={s.navigationList__link} to="/memorialdinners"><li className={s.navigationList__item}>Поминальні обіди</li></Link>
+            <Link className={s.navigationList__link} to="/documentsforfuneral"><li className={s.navigationList__item}>Оформлення документів для похорону</li></Link>
+            <Link className={s.navigationList__link} to="/ritualhall"><li className={s.navigationList__item}>Ритуальна зала</li></Link>
+            <Link className={s.navigationList__link} to="/musicalaccompanimen"><li className={s.navigationList__item}>Музичний супровід</li></Link>
+            <Link className={s.navigationList__link} to="/removalbody"><li className={s.navigationList__item}>Винесення тіла</li></Link>
+            <Link className={s.navigationList__link} to="/requiemservicebypriest"><li className={s.navigationList__item}>Відспівування померлого</li></Link>
+            <Link className={s.navigationList__link} to="/sanitationofpremises"><li className={s.navigationList__item}>Санобробка приміщень</li></Link>
+            <Link className={s.navigationList__link} to="/makingfuneralportrait"><li className={s.navigationList__item}>Виготовлення фотопортрету</li></Link>
+            <Link className={s.navigationList__link} to="/funeralescortbymedicalpersonnal"><li className={s.navigationList__item}>Супровід медперсоналом</li></Link>
         </ul>
         
-        <h2 className={s.navigationList__title}><Link to="/ritualgoods">Ритуальні товари</Link></h2>
+        <Link className={s.navigationList__link} to="/ritualgoods"><h2 className={s.navigationList__title}>Ритуальні товари</h2></Link>
                 
         <ul className={s.navigationList__list}>
-            <li className={s.navigationList__item}><Link to="/ritualwreath">Вінки</Link></li>
-            <li className={s.navigationList__item}><Link to="/сoffins">Труни</Link></li>
-            <li className={s.navigationList__item}><Link to="/crosses">Хрести</Link></li>
-            <li className={s.navigationList__item}><Link to="/ritualmonuments">Пам’ятники</Link></li>
-            <li className={s.navigationList__item}><Link to="/ritualaccessories">Приладдя</Link></li>
+            <Link className={s.navigationList__link} to="/ritualwreath"><li className={s.navigationList__item}>Вінки</li></Link>
+            <Link className={s.navigationList__link} to="/сoffins"><li className={s.navigationList__item}>Труни</li></Link>
+            <Link className={s.navigationList__link} to="/crosses"><li className={s.navigationList__item}>Хрести</li></Link>
+            <Link className={s.navigationList__link} to="/ritualmonuments"><li className={s.navigationList__item}>Пам’ятники</li></Link>
+            <Link className={s.navigationList__link} to="/ritualaccessories"><li className={s.navigationList__item}>Приладдя</li></Link>
         </ul>
 
         </div>
