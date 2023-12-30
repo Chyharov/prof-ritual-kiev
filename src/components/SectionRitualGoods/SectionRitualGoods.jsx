3d652b222d93611dc45a713b1b2905c96fe13b8b
myusername <chyharov@gmail.com>
2023-12-31 01:16:31 +0200

    ADDED: styles on SectionRitualGoods for desktop

--- src/components/SectionRitualGoods/SectionRitualGoods.jsx
+++ src/components/SectionRitualGoods/SectionRitualGoods.jsx
@@ -1,4 +1,4 @@
-import RitualGoodsDecoration from '../../images/RitualGoods/ritualGoodsDecoration.jpg'
+import { Link } from 'react-router-dom';
 import BntMoreDetail from 'components/BntMoreDetail/BntMoreDetail';
 import s from './SectionRitualGoods.module.scss'
 
@@ -7,16 +7,20 @@ import s from './SectionRitualGoods.module.scss'
 const SectionRitualGoods = () => {
     return (
       <section className={s.sectionRitualGoods}>
-            <div className={'container ' + s.ritualGoods__container}>
-                <img className={s.ritualGoods__decoration} loading="lazy" src={RitualGoodsDecoration} alt="ritualGoods__decoration" />
+        <div className={'container ' + s.ritualGoods__container}>
+          <div className={s.ritualGoods__decoration} loading="lazy">
+          </div>
 
-                <h2 className={s.ritualGoods__title}>Ритуальні товари</h2>
+          <div className={s.ritualGoods__DescriprionContainer}>
+            <h2 className={s.ritualGoods__title}>Ритуальні товари</h2>
 
-                <p className='description' style={{marginBottom: '15px'}}>Продажем ритуальних товарів та приладдя наша компанія займається вже <b>більше 10 років</b>. Все ритуальне приладдя, яке ми маємо в наявності, ви можете подивитися відвідавши нас за адресою вул. Перта Запорожця 13 у місті Київ.</p>
+            <p className={s.ritualGoods__description}>Продажем ритуальних товарів та приладдя наша компанія займається вже <b>більше 10 років</b>. Все ритуальне приладдя, яке ми маємо в наявності, ви можете подивитися відвідавши нас за адресою вул. Перта Запорожця 13 у місті Київ.</p>
                 
-                <p className='description' style={{ marginBottom: '35px' }}>В наявності є більше <b>100 різних ритуальних товарів</b>, які ми продаємо за найнижчими цінами у місті Київ. При покупці ритуальної труни або ритуальних вінків ми робимо великі знижки на багато ритуальних речей.</p>
+            <p className={s.ritualGoods__description} style={{ marginBottom: '35px' }}>В наявності є більше <b>100 різних ритуальних товарів</b>, які ми продаємо за найнижчими цінами у місті Київ. При покупці ритуальної труни або ритуальних вінків ми робимо великі знижки на багато ритуальних речей.</p>
                 
-                <BntMoreDetail />
+            <Link to="/ritualgoods"><BntMoreDetail /></Link>
+          </div>
+                
         </div>
       </section>
   )
