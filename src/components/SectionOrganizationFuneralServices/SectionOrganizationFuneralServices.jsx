f6ea31235a5fc8b80b589ffe36f6d1e7fd05af19
myusername <chyharov@gmail.com>
2023-12-31 00:25:58 +0200

    ADDED: styles on SectionOrganizationFuneralServices for desktop

--- src/components/SectionOrganizationFuneralServices/SectionOrganizationFuneralServices.jsx
+++ src/components/SectionOrganizationFuneralServices/SectionOrganizationFuneralServices.jsx
@@ -1,4 +1,4 @@
-import OrganizationFuneral from '../../images/OrganizationFuneralServices/OrganizationFuneralServices.jpg'
+import { Link } from 'react-router-dom';
 import BntMoreDetail from 'components/BntMoreDetail/BntMoreDetail';
 import s from './SectionOrganizationFuneralServices.module.scss'
 
@@ -8,15 +8,19 @@ const SectionOrganizationFuneralServices = () => {
     return (
       <section className={s.sectionOrganizationFuneralServices}>
         <div className={'container ' + s.funeralServices__container}>
-                <img className={s.funeralServices__decoration} loading="lazy" src={OrganizationFuneral} alt="hero__decoration" />
+          <div loading="lazy" className={s.funeralServices__decoration}>
+          </div>
                 
-                <h2 className={s.funeralServices__title}>Організація ритуальних послуг</h2>
+          <div className={s.organizationFuneralServicesInfoContainer}>
+              <h2 className={s.funeralServices__title}>Організація ритуальних послуг</h2>
                 
-                <p className={s.funeralServices__description}>Починаючи з оформлення документів і закінчуючи поминальним обідом. Такий стрес може негативно вплинути на здоров’я людини. Профритуал більше 10 років працює у сфері ритуальних послуг і ми знаємо як організувати похорон за <b>доступною для вас ціною.</b></p>
+              <p className={s.funeralServices__description}>Починаючи з оформлення документів і закінчуючи поминальним обідом. Такий стрес може негативно вплинути на здоров’я людини. Профритуал більше 10 років працює у сфері ритуальних послуг і ми знаємо як організувати похорон за <b>доступною для вас ціною.</b></p>
                 
-                <p className={s.funeralServices__description} style={{marginBottom: '35px'}}>Ми працюємо з усіма клієнтами і знаходимо їм найкращий варіант. Ми співпрацюємо з <b>десятками ресторанів</b>, маємо власний транспорт, тому ціна наших ритуальних послуг завжди залишається низькою і доступною для всіх.</p>
+              <p className={s.funeralServices__description} style={{marginBottom: '35px'}}>Ми працюємо з усіма клієнтами і знаходимо їм найкращий варіант. Ми співпрацюємо з <b>десятками ресторанів</b>, маємо власний транспорт, тому ціна наших ритуальних послуг завжди залишається низькою і доступною для всіх.</p>
 
-                <BntMoreDetail />
+              <Link to="/ritualservices"><BntMoreDetail /></Link>
+          </div>
+                
         </div>
       </section>
   )
