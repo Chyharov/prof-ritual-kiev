d3a2f743b5abbb97a6ffbf87db402920271cca48
myusername <chyharov@gmail.com>
2024-01-05 00:45:40 +0200

    ADDED: styles and markup on SectionAbooutRemovalBody for desktop

--- src/components/SectionAbooutRemovalBody/SectionAbooutRemovalBody.jsx
+++ src/components/SectionAbooutRemovalBody/SectionAbooutRemovalBody.jsx
@@ -1,49 +1,72 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Modal from '../ModalWindow/ModalWindow';
 import RemovalBodyDecoration from '../../images/RemovalBody/RemovalBodyDecoration.jpg'
-import funeralTeam__photo from '../../images/RemovalBody/funeralTeam__photo.jpeg'
-import funeralTeam__photo2 from '../../images/RemovalBody/funeralTeam__photo2.jpeg'
-import funeralTeam__photo3 from '../../images/RemovalBody/funeralTeam__photo3.jpeg'
-import SectionNeedHelp from 'components/SectionNeedHelp/SectionNeedHelp';
 import s from './SectionAbooutRemovalBody.module.scss'
 
+const removalBodyImgListArray = [
+    { id: 1, src: require('../../images/RemovalBody/funeralTeam__photo.jpeg'), alt: 'Зображення 1' },
+    { id: 2, src: require('../../images/RemovalBody/funeralTeam__photo2.jpeg'), alt: 'Зображення 2' },
+    { id: 3, src: require('../../images/RemovalBody/funeralTeam__photo3.jpeg'), alt: 'Зображення 3' },
+];
+
 const SectionAbooutRemovalBody = () => {
+
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const openModal = id => {
+        setSelectedImage(removalBodyImgListArray.find(image => image.id === id));
+        setModalOpen(true);
+        document.body.style.overflow = 'hidden';
+  };
+
     return (
       <section className={s.sectionAbooutRemovalBody}>
         <div className={'container ' + s.abooutRemovalBody__container}>
                 
             <img className={s.abooutRemovalBody__decoration} loading="lazy" src={RemovalBodyDecoration} alt="abooutRemovalBody__decoration" />
 
-            <p className={s.abooutRemovalBody__description}>Наше ритуальне бюро знає, як складно в таку хвилину думати про <b>дотримання всіх правил</b>, пов’язаних з винесенням тіла померлого, саме тому ми пропонуємо вам скористатися послугою – бригада для похорону. Наші співробітники добре знають усі правила винесення тіла – як за православними правилами, так і відповідно до звичаїв та правил інших релігій.</p>                
+            <p style={{marginBottom: '10px'}} className='description'>Наше ритуальне бюро знає, як складно в таку хвилину думати про <b>дотримання всіх правил</b>, пов’язаних з винесенням тіла померлого, саме тому ми пропонуємо вам скористатися послугою – бригада для похорону. Наші співробітники добре знають усі правила винесення тіла – як за православними правилами, так і відповідно до звичаїв та правил інших релігій.</p>                
 
-            <h2 className={s.abooutRemovalBody__title}>Бригада ритуальних вантажників</h2>
+            <h2 style={{marginBottom: '10px'}} className='title'>Бригада ритуальних вантажників</h2>
                 
-            <blockquote className={s.abooutRemovalBody__blockquote}> Вона працюватиме від початку самого процесу похорону і до його завершення. Склад бригади залежатиме від розміру труни та складності роботи, є мінімальний, стандартний та елітний склад <b>(4-6-8 осіб)</b>.</blockquote>
+            <blockquote style={{marginBottom: '10px'}} className='blockquote'> Вона працюватиме від початку самого процесу похорону і до його завершення. Склад бригади залежатиме від розміру труни та складності роботи, є мінімальний, стандартний та елітний склад <b>(4-6-8 осіб)</b>.</blockquote>
             
-            <p style={{marginBottom: '20px'}} className={s.abooutRemovalBody__description}>Якщо Вам потрібно виконати перевезення тіла померлого на тривалі відстані, ритуальні вантажники проводять разом з вами тіло до вокзалу або аеропорту. Крім того, за вашим бажанням вони можуть брати участь у <Link className={s.abooutRemovalBody__link} to="/cargo">далеких перевезеннях померлих</Link></p>
-
-                <SectionNeedHelp />
+            <p style={{marginBottom: '10px'}} className='description'>Якщо Вам потрібно виконати перевезення тіла померлого на тривалі відстані, ритуальні вантажники проводять разом з вами тіло до вокзалу або аеропорту. Крім того, за вашим бажанням вони можуть брати участь у <Link className='links' to="/cargo">далеких перевезеннях померлих</Link></p>
                 
-            <h2 style={{ marginTop: '20px' }} className={s.abooutRemovalBody__title}>Наша Бригада для похорону</h2>    
+            <h2 style={{marginBottom: '10px'}} className='title'>Наша Бригада для похорону</h2>    
 
-            <p className={s.abooutRemovalBody__description}>Багато людей вважають за краще не замовляти професійну бригаду для винесення тіла померлого. Вони довіряють цей ритуал похорону членам сім’ї чи близьким знайомим. Але на жаль, люди журяться втратою друга чи члена сім’ї по-різному. Але ваші близькі та друзі теж перебувають під стресом, їхній друг помер, його більше немає. Щоб пом’якшити горе перед похороном багато хто вживає алкоголь. Не знаючи коректної процедури винесення тіла померлого і <b>під впливом алкоголю</b> часто трапляються нещасні випадки під час похорону, які завдають незворотного стресу близьким і рідним померлого. Наша бригада це міцні та сильні чоловіки, які відповідально ставляться до своєї роботи.</p>
+            <p style={{marginBottom: '10px'}} className='description'>Багато людей вважають за краще не замовляти професійну бригаду для винесення тіла померлого. Вони довіряють цей ритуал похорону членам сім’ї чи близьким знайомим. Але на жаль, люди журяться втратою друга чи члена сім’ї по-різному. Але ваші близькі та друзі теж перебувають під стресом, їхній друг помер, його більше немає. Щоб пом’якшити горе перед похороном багато хто вживає алкоголь. Не знаючи коректної процедури винесення тіла померлого і <b>під впливом алкоголю</b> часто трапляються нещасні випадки під час похорону, які завдають незворотного стресу близьким і рідним померлого. Наша бригада це міцні та сильні чоловіки, які відповідально ставляться до своєї роботи.</p>
 
-            <h4 className={s.abooutRemovalBody__titleSmal}>Клієнти, які замовляли ритуальну послугу “бригада для похорону” так само цікавилися:</h4>
+            <h3 style={{marginBottom: '10px'}} className='titleSmall'>Клієнти, які замовляли ритуальну послугу “бригада для похорону” так само цікавилися:</h3>
                 
-            <ul style={{ marginBottom: '20px' }} className={s.abooutRemovalBody__list}>
-                <li><a className={s.abooutRemovalBody__link} href="/">Орендою ритуального залу</a></li>
-                <li><a className={s.abooutRemovalBody__link} href="/">Оформлення документів</a></li>
-                <li><a className={s.abooutRemovalBody__link} href="/">Ритуальний транспорт</a></li>
-                <li><a className={s.abooutRemovalBody__link} href="/">Поминальний обід у Києві</a></li>  
+            <ul style={{ marginBottom: '20px' }} className='list'>
+                <li style={{marginBottom: '10px'}}><Link className='links' href="/">Орендою ритуального залу</Link></li>
+                <li style={{marginBottom: '10px'}}><Link className='links' href="/">Оформлення документів</Link></li>
+                <li style={{marginBottom: '10px'}}><Link className='links' href="/">Ритуальний транспорт</Link></li>
+                <li style={{marginBottom: '10px'}}><Link className='links' href="/">Поминальний обід у Києві</Link></li>  
             </ul>
                 
-            <h4 className={s.abooutRemovalBody__titleSmal}>Фото нашої бригади для похорону</h4>
-                
-            <ul className={s.funeralTeam__photoList}>
-                <li className={s.funeralTeam__photoItem}><img className={s.funeralTeam__photo} loading="lazy" src={funeralTeam__photo} alt="funeralTeam__photo" /></li>
-                <li className={s.funeralTeam__photoItem}><img className={s.funeralTeam__photo} loading="lazy" src={funeralTeam__photo2} alt="funeralTeam__photo2" /></li>
-                <li className={s.funeralTeam__photoItem}><img className={s.funeralTeam__photo} loading="lazy" src={funeralTeam__photo3} alt="funeralTeam__photo3" /></li>
+            <h3 style={{marginBottom: '20px'}} className='titleSmall'>Фото нашої бригади для похорону</h3>
+
+            <ul style={{marginBottom: '20px'}} className={s.funeralTeam__photoList}>
+                {removalBodyImgListArray.map((image) => (        
+                    <li style={{marginBottom: '10px'}} key={image.id}>
+                        <img className={s.funeralTeam__photo} loading="lazy" id={image.id} src={image.src} alt={image.alt} onClick={() => openModal(image.id, image.src, image.alt)}/>
+                    </li>
+                ))}
             </ul>
-                
+
+            {modalOpen && selectedImage && (
+                <Modal
+                selectedImage={selectedImage}
+                arrayPhoto={removalBodyImgListArray}
+                setSelectedImage={setSelectedImage}
+                setModalOpen={setModalOpen}
+                />
+            )}
+
         </div>
       </section>
   )
--- src/pages/RemovalBodyPage/RemovalBodyPage.jsx
+++ src/pages/RemovalBodyPage/RemovalBodyPage.jsx
@@ -2,6 +2,7 @@ import Header from 'components/Header/Header';
 import SectionContactsList from 'components/SectionContactsList/SectionContactsList';
 import SectionCurrentPage from 'components/SectionCurrentPage/SectionCurrentPage';
 import SectionAbooutRemovalBody from 'components/SectionAbooutRemovalBody/SectionAbooutRemovalBody';
+import SectionNeedHelp from 'components/SectionNeedHelp/SectionNeedHelp';
 import SectionNavigationList from 'components/SectionNavigationList/SectionNavigationList';
 import SectionContacts from 'components/SectionContacts/SectionContacts';
 import SectionRitualServicesList from 'components/SectionRitualServicesList/SectionRitualServicesList';
@@ -18,6 +19,7 @@ const RemovalBodyPage = () => {
             <SectionContactsList />
             <SectionCurrentPage title={RemovalBodyPageTitle} />
             <SectionAbooutRemovalBody />
+            <SectionNeedHelp />
             <SectionNavigationList />
             <SectionContacts />
             <SectionRitualServicesList />
