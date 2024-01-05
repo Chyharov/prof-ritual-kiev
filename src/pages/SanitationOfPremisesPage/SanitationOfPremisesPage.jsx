c60f560d7a1be81f57733d385c06454dfa599d16
myusername <chyharov@gmail.com>
2024-01-05 21:49:53 +0200

    ADDED: markup and styles on SectionAboutSanitationOfPremises for desktop

--- src/pages/SanitationOfPremisesPage/SanitationOfPremisesPage.jsx
+++ src/pages/SanitationOfPremisesPage/SanitationOfPremisesPage.jsx
@@ -2,6 +2,7 @@ import Header from 'components/Header/Header';
 import SectionContactsList from 'components/SectionContactsList/SectionContactsList';
 import SectionCurrentPage from 'components/SectionCurrentPage/SectionCurrentPage';
 import SectionAboutSanitationOfPremises from 'components/SectionAboutSanitationOfPremises/SectionAboutSanitationOfPremises';
+import SectionNeedHelp from 'components/SectionNeedHelp/SectionNeedHelp';
 import SectionNavigationList from 'components/SectionNavigationList/SectionNavigationList';
 import SectionContacts from 'components/SectionContacts/SectionContacts';
 import SectionRitualServicesList from 'components/SectionRitualServicesList/SectionRitualServicesList';
@@ -18,6 +19,7 @@ const SanitationOfPremisesPage = () => {
             <SectionContactsList />
             <SectionCurrentPage title={SanitationOfPremisesPageTitle} />
             <SectionAboutSanitationOfPremises />
+            <SectionNeedHelp />
             <SectionNavigationList />
             <SectionContacts />
             <SectionRitualServicesList />
